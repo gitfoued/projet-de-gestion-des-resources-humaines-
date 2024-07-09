9972ed57673a5484545763b7376c137362eff758
unknown <fouedtrabelsu2002@gmail.com>
2024-07-09 12:22:15 +0100

    ajout

--- server/src/main/java/com/example/server/auth/AuthenticationController.java
+++ server/src/main/java/com/example/server/auth/AuthenticationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @ResponseBody
 public class AuthenticationController {
@@ -16,12 +16,14 @@ public class AuthenticationController {
     private final AuthenticationService service;
 
     @PostMapping("/register")
+    @CrossOrigin(origins="http://localhost:3000")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
     @PostMapping("/Login")
+    @CrossOrigin(origins="http://localhost:3000")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
